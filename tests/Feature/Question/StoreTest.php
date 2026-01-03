52786c778102f76a225b358e4f02fead151bb522
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2026-01-03 16:56:41 -0300

    API-2: after creating a new question, I need to make sure that it creates on _draft_ status

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -19,3 +19,19 @@ it('should be able to store a question', function () {
         'question' => 'Lorem ipsum jeremias?',
     ]);
 });
+
+test('after creating a new question, I need to make sure that it creates on _draft_ status', function () {
+    $user = User::factory()->createOne();
+
+    Sanctum::actingAs($user);
+
+    postJson(route('questions.store'), [
+        'question' => 'Lorem ipsum jeremias?',
+    ]);
+
+    assertDatabaseHas('questions', [
+        'user_id'  => $user->id,
+        'status'   => 'draft',
+        'question' => 'Lorem ipsum jeremias?',
+    ]);
+});
