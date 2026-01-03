68324dbb54d5d7a62b3096a67b920054220025d8
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2026-01-03 16:14:41 -0300

    API-2: it should be able to store a question

---
+++ app/Http/Controllers/Question/StoreController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Question;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Question\StoreRequest;
+use App\Models\Question;
+
+class StoreController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(StoreRequest $request)
+    {
+        Question::create([
+            'user_id'  => auth()->id(),
+            'question' => $request->question,
+        ]);
+
+        return response()->noContent();
+    }
+}
---
+++ tests/Feature/Question/StoreTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
+use function Pest\Laravel\{assertDatabaseHas, postJson};
+
+it('should be able to store a question', function () {
+    $user = User::factory()->createOne();
+
+    Sanctum::actingAs($user);
+
+    postJson(route('questions.store'), [
+        'question' => 'Lorem ipsum jeremias?',
+    ]);
+
+    assertDatabaseHas('questions', [
+        'user_id'  => $user->id,
+        'question' => 'Lorem ipsum jeremias?',
+    ]);
+});
