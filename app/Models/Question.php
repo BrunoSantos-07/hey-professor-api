3a1a17adc66c90ff904123cba2497ebadb0f0734
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2026-01-06 22:13:08 -0300

    API-23: arquive a question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Model, SoftDeletes};
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Question extends Model
 {
     /** @use HasFactory<\Database\Factories\QuestionFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'questions';
 
---
+++ tests/Feature/Question/ArchiveTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\{Question, User};
+use Laravel\Sanctum\Sanctum;
+
+use function Pest\Laravel\{assertNotSoftDeleted, assertSoftDeleted, deleteJson};
+
+it('should be able to archive a question', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user)->create();
+
+    Sanctum::actingAs($user);
+
+    deleteJson(route('questions.archive', $question))
+        ->assertNoContent();
+
+    assertSoftDeleted('questions', ['id' => $question->id]);
+});
+
+it('should allow that only the creator can archive', function () {
+    $user1    = User::factory()->create();
+    $user2    = User::factory()->create();
+    $question = Question::factory()->for($user1)->create();
+
+    Sanctum::actingAs($user2);
+
+    deleteJson(route('questions.archive', $question))
+        ->assertForbidden();
+
+    assertNotSoftDeleted('questions', ['id' => $question->id]);
+});
