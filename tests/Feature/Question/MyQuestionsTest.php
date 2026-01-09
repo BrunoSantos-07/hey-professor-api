e715c89022f401604bc61b708601303985028b34
BrunoSantos-07 <brunosantos.dev07@gmail.com>
2026-01-09 16:48:46 -0300

    API-25: it should list only questions that the logged user has been created :: draft

--- tests/Feature/Question/MyQuestionsTest.php
+++ tests/Feature/Question/MyQuestionsTest.php
@@ -33,3 +33,32 @@ it('should list only questions that the logged user has been created :: publishe
         'question' => $anotherUserQuestion->question,
     ]);
 });
+
+it('should list only questions that the logged user has been created :: draft', function () {
+    $user         = User::factory()->create();
+    $userQuestion = Question::factory()->draft()->for($user)->create();
+
+    $anotherUserQuestion = Question::factory()->draft()->create();
+
+    Sanctum::actingAs($user);
+
+    $request = getJson(route('my-questions', ['status' => 'draft']))
+        ->assertOk();
+
+    $request->assertJsonFragment([
+        'id'         => $userQuestion->id,
+        'question'   => $userQuestion->question,
+        'status'     => $userQuestion->status,
+        'created_by' => [
+            'id'   => $userQuestion->user->id,
+            'name' => $userQuestion->user->name,
+        ],
+        'created_at' => $userQuestion->created_at->format('Y-m-d h:i:s'),
+        'updated_at' => $userQuestion->updated_at->format('Y-m-d h:i:s'),
+        //TODO: add like and unlike
+
+    ])->assertJsonMissing([
+        'id'       => $anotherUserQuestion->id,
+        'question' => $anotherUserQuestion->question,
+    ]);
+});
